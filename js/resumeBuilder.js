34a5a8ab4c1466bb5daa01400e200ca9845b9ca4
Jason Demetri <jsd219@gmail.com>
2015-01-14 15:53:00 -0600

    Lesson 2

---
+++ js/resumeBuilder.js
@@ -0,0 +1,51 @@
+var bio = {
+	"name" : formattedName,
+	"role" : formattedRole,
+	"contacts" : {
+		"mobile":"123-123-1234",
+		"email":"jsd219@gmail.com",
+		"github":"johndoe",
+		"twitter":"@johndoe",
+		"location":"Austin Texas"
+	},
+	"picURL" : "images/fry.jpg",
+	"welcomeMessage" : "Hello, my name is Jason and this si my welcome message.",  
+	"skills" : ["creative", "hard worker", "js", "php"]
+}
+
+var work = {};
+work.position = "Full stack developer";
+work.employer = "PGi";
+work.years = "5";
+
+var education = {
+	"schools": [
+		{
+			"name": "ACC",
+			"years": "2",
+			"major": "English"
+		},
+		{
+			"name": "Hard Knox",
+			"years": "10",
+			"major": "programing"
+		}
+	]
+}
+
+var projects = {
+	"project 1": [
+		{
+			"title": "jammin jack",
+			"dates": "2010-2012",
+			"description": "Built a jammin jack robot out of wood and electric motors"
+		}
+	],
+	"project 2": [
+		{
+			"title": "batty betty",
+			"dates": "2013-2015",
+			"description": "works out problems with house cleaning chore assignments"
+		}
+	]
+}
